b55ccee86a2970ffec0b0ebc485e55aaf37d8ab4
Fernando <fernandorato.0@hotmail.com>
2020-01-23 17:34:11 -0300

    Adicionado Estilos Globais

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './config/ReactotronConfig';
 import { Router } from 'react-router-dom';
 import history from '~/services/history';
+import GlobalStyles from '~/styles/GlobalStyles';
 
 import Routes from '~/routes';
 
@@ -9,6 +10,7 @@ function App() {
     return (
         <Router history={history}>
             <Routes />
+            <GlobalStyles />
         </Router>
     );
 }
