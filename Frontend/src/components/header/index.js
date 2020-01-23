4ce27c6654513c7099881ccabebd61f83f4ce340
Fernando <fernandorato.0@hotmail.com>
2020-01-23 19:54:53 -0300

    Estilizado header

--- Frontend/src/components/header/index.js
+++ Frontend/src/components/header/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Wrapper, Content, Botoes } from './styles';
+
+import logo from '~/assets/logomin.svg';
 
 export default function header() {
     return (
-        <div />
+        <Wrapper>
+            <Content>
+                <div>
+                    <Link to="/">
+                        <img src={logo} alt="gymlogo" />
+                    </Link>
+                    <Botoes>
+                        <Link to="/alunos">
+                            <p>ALUNOS</p>
+                        </Link>
+
+                        <Link to="/planos">
+                            <p>PLANOS</p>
+                        </Link>
+
+                        <Link to="/matriculas">
+                            <p>MATRÌCULAS</p>
+                        </Link>
+
+                        <Link to="/auxilioalunos">
+                            <p>PEDIDOS DE AUXÌLIO</p>
+                        </Link>
+                    </Botoes>
+                </div>
+
+                <aside>
+                    <p>Fernando Santos</p>
+                    <button type="button">Sair do Sistema</button>
+                </aside>
+            </Content>
+        </Wrapper>
     );
 }
--- Frontend/src/components/header/styles.js
+++ Frontend/src/components/header/styles.js
@@ -1,3 +1,65 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Container = styled.div``; 
+export const Wrapper = styled.div`
+    height: 64px;
+    padding: 0 30px;
+    background: #fff;
+`;
+
+export const Content = styled.div`
+    max-width: 1400px;
+    margin: 0 auto;
+    height: 64px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    div {
+        display: flex;
+        align-items: center;
+
+        img {
+            padding-right: 30px;
+            border-right: 1px solid #ddd;
+        }
+    }
+
+    aside {
+        text-align: right;
+        p {
+            font-size: 14px;
+            color: #666;
+        }
+
+        button {
+            color: #de3b3b;
+            border: none;
+            background: none;
+            font-size: 13px;
+            transition: font-size 0.2s;
+
+            &:hover {
+                font-size: 14px;
+            }
+        }
+    }
+`;
+export const Botoes = styled.div`
+    margin-left: 30px;
+
+    a {
+        font-size: 15px;
+        font-weight: bold;
+        color: #999;
+
+        & + a {
+            padding-left: 20px;
+        }
+
+        transition: color 0.2s;
+
+        &:hover {
+            color: #444;
+        }
+    }
+`;
