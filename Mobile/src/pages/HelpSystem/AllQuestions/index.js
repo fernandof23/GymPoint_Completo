e4131f91361554f2b2b489ca2e0714bbf5147f75
Fernando <fernandorato.0@hotmail.com>
2020-02-17 15:39:43 -0300

    puxando perguntas da api

--- Mobile/src/pages/HelpSystem/AllQuestions/index.js
+++ Mobile/src/pages/HelpSystem/AllQuestions/index.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { Text } from 'react-native';
+/* eslint-disable prettier/prettier */
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { formatDistance, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import api from '~/services/api';
 
 import Background from '~/components/Background';
 import Header from '~/components/Header';
 
+import { Wrapper, ButtonCheckIn, Content } from './styles';
+
 export default function AllQuestions() {
+    const [questions, setQuestions] = useState([]);
+
+    const user = useSelector(state => state.user.profile);
+
+    useEffect(() => {
+        const { id } = user;
+        async function loadQuestion() {
+            const response = await api.get(`/students/${id}/help-orders`);
+
+            const data = response.data.map(question => ({
+                ...question,
+                dateDistance: question.answer
+                    ? formatDistance(parseISO(question.updatedAt), new Date(), {
+                        addSuffix: true,
+                        locale: pt,
+                    })
+                    : formatDistance(parseISO(question.createdAt), new Date(), {
+                        addSuffix: true,
+                        locale: pt,
+                    }),
+            }));
+
+            setQuestions(data);
+        }
+        loadQuestion();
+    }, [user, user.id]);
+
+    console.tron.log(questions);
     return (
         <Background>
-            <Text>Teste</Text>
+            <Wrapper>
+                <ButtonCheckIn onPress={() => { }}>
+                    Novo Pedido de auxílio
+                </ButtonCheckIn>
+                <Content />
+            </Wrapper>
         </Background>
     );
 }
---
+++ Mobile/src/pages/HelpSystem/AllQuestions/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+import Button from '~/components/Button';
+
+export const Wrapper = styled.View`
+    flex: 1;
+    padding: 20px;
+`;
+
+export const ButtonCheckIn = styled(Button)`
+    margin-bottom: 20px;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    background: #f5f5f5;
+`;
