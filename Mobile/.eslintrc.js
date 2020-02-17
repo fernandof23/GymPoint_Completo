2584f46a72d73ca67108df7a73d18914b578d5e9
Fernando <fernandorato.0@hotmail.com>
2020-02-17 09:25:27 -0300

    Adicionado Eslint

--- Mobile/.eslintrc.js
+++ Mobile/.eslintrc.js
@@ -1,4 +1,51 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    env: {
+        es6: true,
+    },
+    extends: [
+        'airbnb',
+        'prettier',
+        'prettier/react'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        __DEV__: true,
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        'prettier',
+        'jsx-a11y',
+        'import',
+        'react-hooks',
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.jsx', 'js'] }
+        ],
+        'import/prefer-default-export': 'off',
+        'react/state-in-constructor': 'off',
+        'react/static-property-placement': 'off',
+        "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+        "react/jsx-one-expression-per-line": "off",
+        "global-require": "off",
+        "react-native/no-raw-text": "off",
+        "no-param-reassign": "off",
+        "no-underscore-dangle": "off",
+        camelcase: "off",
+        "no-console": ["error", { allow: ["tron"] }],
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "prefer-object-spread": "off"
+    },
 };
