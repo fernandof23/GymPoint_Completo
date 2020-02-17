3453ec7fcc655edacca9e118fe22b4de3c44bccf
Fernando <fernandorato.0@hotmail.com>
2020-02-17 16:13:02 -0300

    Listagem de perguntas pronto

--- Mobile/src/pages/HelpSystem/AllQuestions/index.js
+++ Mobile/src/pages/HelpSystem/AllQuestions/index.js
@@ -3,13 +3,14 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { formatDistance, parseISO } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import api from '~/services/api';
 
 import Background from '~/components/Background';
 import Header from '~/components/Header';
 
-import { Wrapper, ButtonCheckIn, Content } from './styles';
+import { Wrapper, ButtonCheckIn, Content, HeaderContent, Reply, Time, Question, ReplyContent, ContentList } from './styles';
 
 export default function AllQuestions() {
     const [questions, setQuestions] = useState([]);
@@ -39,6 +40,8 @@ export default function AllQuestions() {
         loadQuestion();
     }, [user, user.id]);
 
+
+
     console.tron.log(questions);
     return (
         <Background>
@@ -46,7 +49,29 @@ export default function AllQuestions() {
                 <ButtonCheckIn onPress={() => { }}>
                     Novo Pedido de auxílio
                 </ButtonCheckIn>
-                <Content />
+                <Content
+                    data={questions}
+                    keyExtractor={question => question.id}
+                    renderItem={({ item }) => (
+                        <ContentList onPress={() => { }}>
+                            <HeaderContent>
+                                <ReplyContent>
+                                    <Icon name="check-circle" color={item.answer ? "#42CB59" : "#999999"} size={14} />
+                                    <Reply answer={item.answer}>{item.answer ? "Respondido" : "Sem Resposta"}</Reply>
+                                </ReplyContent>
+                                <Time>
+                                    {item.dateDistance}
+                                </Time>
+                            </HeaderContent>
+                            <Question>
+                                {item.question}
+                            </Question>
+                        </ContentList>
+                    )}
+                />
+
+
+
             </Wrapper>
         </Background>
     );
--- Mobile/src/pages/HelpSystem/AllQuestions/styles.js
+++ Mobile/src/pages/HelpSystem/AllQuestions/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 import Button from '~/components/Button';
 
 export const Wrapper = styled.View`
-    flex: 1;
     padding: 20px;
 `;
 
@@ -10,7 +10,42 @@ export const ButtonCheckIn = styled(Button)`
     margin-bottom: 20px;
 `;
 
-export const Content = styled.View`
-    flex: 1;
-    background: #f5f5f5;
+export const Content = styled.FlatList.attrs({
+    showsVerticalScrollIndicator: false,
+})``;
+
+export const ContentList = styled(RectButton)`
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+    margin-bottom: 10px;
+`;
+
+export const HeaderContent = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    padding-bottom: 15px;
+`;
+
+export const ReplyContent = styled.View`
+    flex-direction: row;
+
+    align-items: center;
+`;
+export const Reply = styled.Text`
+    font-size: 14px;
+    font-weight: bold;
+    color: #999;
+    margin-left: 5px;
+`;
+
+export const Time = styled.Text`
+    font-size: 14px;
+    text-align: right;
+    color: #666;
+`;
+
+export const Question = styled.Text`
+    font-size: 14px;
+    color: #666;
 `;
