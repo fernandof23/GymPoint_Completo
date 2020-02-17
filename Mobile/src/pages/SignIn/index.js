4429cca500897f94a2441bc2f8e899e179d6bf12
Fernando <fernandorato.0@hotmail.com>
2020-02-17 10:50:01 -0300

    Signin style pronto

--- Mobile/src/pages/SignIn/index.js
+++ Mobile/src/pages/SignIn/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import { Container, Image } from './styles';
+import {
+    Container,
+    Image,
+    Form,
+    FormInput,
+    SubmitButton,
+    ContainerInput,
+} from './styles';
 
 import logo from '~/assets/logo.png';
-import Button from '~/components/Button';
 
 export default function SignIn() {
+    const [idLogin, setIdLogin] = useState('');
+
+    const loading = useSelector(state => state.auth.loading);
+
+    function handleSubmit() { }
+
     return (
         <Container>
             <Image source={logo} />
+
+            <Form>
+                <ContainerInput>
+                    <FormInput
+                        autoCaptalize="none"
+                        placeholder="Informe seu ID de cadastro"
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
+                        value={idLogin}
+                        onChangeText={setIdLogin}
+                    />
+                </ContainerInput>
+
+                <SubmitButton loading={loading} onPress={handleSubmit}>
+                    Entrar no Sistema
+                </SubmitButton>
+            </Form>
         </Container>
     );
 }
--- Mobile/src/pages/SignIn/styles.js
+++ Mobile/src/pages/SignIn/styles.js
@@ -1,9 +1,38 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import Button from '~/components/Button';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView.attrs({
+    enabled: Platform.OS === 'ios',
+    behavior: 'padding',
+})`
     flex: 1;
     align-items: center;
     justify-content: center;
+    padding: 0 30px;
 `;
 
 export const Image = styled.Image``;
+
+export const Form = styled.View`
+    align-self: stretch;
+    margin-top: 50px;
+`;
+
+export const SubmitButton = styled(Button)`
+    margin-top: 5px;
+`;
+
+export const ContainerInput = styled.View`
+    padding: 0 15px;
+    height: 46px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    margin-bottom: 10px;
+`;
+export const FormInput = styled.TextInput`
+    flex: 1;
+    font-size: 15px;
+    margin-left: 10px;
+    color: #999;
+`;
